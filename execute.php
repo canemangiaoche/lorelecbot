d013e085b79b753af8b81e861b259a35e5bb0c8a
canemangiaoche <45541931+canemangiaoche@users.noreply.github.com>
2018-12-04 00:57:25 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -127,7 +127,7 @@ elseif( strpos(strtolower($text), "aaaa") !== false )
 }
 elseif( strpos(strtolower($text), "/fusione") !== false )
 {
-   $double = explode("/", substr($text, 8));
+   $double = explode("/", substr($text, 9));
    $len0 = (strlen($double[0]) / 2);
    $len1 = -(strlen($double[1]) / 2);
    $response = substr($double[0], $len0).substr($double[1], $len1);
